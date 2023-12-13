b87b679deff77a49e86cfbbd31ead2ce676d57a1
Ytfatan <140451428+Ytfatan@users.noreply.github.com>
2023-12-13 13:42:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul')
+
+menuToggle.addEventListener('click', function () {
+	nav.classList.toggle('slide');
+});
+
+console.log('ini adalah navbar dan hamburger menu yang berasal dari https://youtu.be/a6-v_0kjYyY?si=BeSvjNV3sm1I0Gfw')
